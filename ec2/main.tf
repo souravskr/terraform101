50a54ec4ed590f7171ff3acea954155cf8bc9e2e
Sourav Sarker <souravsker@gmail.com>
2021-05-04 17:14:53 -0230

    created instance with terraform

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = "ca-central-1"
 }
 
+
+variable "instanceName" {
+  type = string
+  description = "Please write the instance name"
+}
+
 variable "ami" {
     type = string
     description = "Fill the ami for your instance"
@@ -15,4 +21,11 @@ variable "instanceType" {
 resource "aws_instance" "ec2instance" {
   ami = var.ami
   instance_type = var.instanceType
+  tags = {
+    Name = var.instanceName
+  }
+}
+
+output "instanceID" {
+    value = aws_instance.ec2instance.id
 }
