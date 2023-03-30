7ab1e0cce92b9a98a229275b3d31c7b3bbe787cf
Sourav Sarker <sourav.sarker@sequencebio.com>
2023-03-30 19:38:00 -0230

    Nested loop

---
+++ Nested Loop/main.tf
@@ -0,0 +1,18 @@
+locals {
+  key_arn_names = {
+    "arn:aws:secretsmanager:region:aws_account_id:secret" = [ "SALESFORCE_HOST", "SALESFORCE_USERNAME", "SALESFORCE_PASSWORD", "SALESFORCE_SECURITY_TOKEN",
+      "SALESFORCE_CLIENT_ID", "SALESFORCE_CLIENT_SECRET", "BASE_URL", "AWS_REGION"]
+  }
+  ecs_secrets = [
+    for key_arn, key_names in local.key_arn_names : [
+      for key_name in key_names:{
+        name      = key_name
+        valueFrom = "${key_arn}:${key_name}::"
+      }
+    ]
+  ]
+}
+
+output "secrets" {
+        value = local.ecs_secrets[0]
+}
